2b46937c32f8750e3e89f1be76dfe5bc3137d06f
Reed Jones <reedjones@reedjones.com>
2017-12-17 14:56:44 -0700

    Add bees

---
+++ app/ts/main.ts
@@ -0,0 +1,140 @@
+const workspace = document.getElementById('workspace') as HTMLElement
+const number_of_bees = 10
+
+// div generation helper
+let makeDiv = (baseClass: string) => {
+  let tmp = document.createElement('div')
+  tmp.className = baseClass
+  return tmp
+}
+
+// bee class
+class Bee {
+  beeNode: HTMLElement
+  maxX: number
+  minX: number
+  maxY: number
+  minY: number
+  translateX: number
+  translateY: number
+  rotation: number
+  timer: number
+  speed: number
+  constructor(bee: HTMLElement, workspace: HTMLElement) {
+    this.beeNode = bee
+    this.maxX = workspace.clientWidth - 75
+    this.minX = 20
+    this.maxY = workspace.clientHeight - 75
+    this.minY = 20
+    this.translateX = Math.random() * this.maxX
+    this.translateY = Math.random() * this.maxY
+    this.rotation = Math.random() * 360
+    this.speed = 0
+    this.createBee()
+  }
+  createBee() {
+    // create head
+    this.beeNode.appendChild(makeDiv('bee-head'))
+
+    // create body
+    let body = makeDiv('bee-body')
+    body.appendChild(makeDiv('bee-wing-left'))
+    body.appendChild(makeDiv('bee-wing-right'))
+    this.beeNode.appendChild(body)
+
+    // set initial position
+    this.transform()
+
+    // display finalized bee
+    this.beeNode.style.display = 'block'
+  }
+  move(s: number, r: number) {
+    this.stop()
+    // set random bee fast speed
+    this.speed = Math.random() * s
+
+    //
+    this.timer = window.setInterval(() => {
+      this.rotation += Math.random() * r - r / 2
+
+      this.calculatePosition()
+
+      this.checkWallCollisions()
+
+      this.transform()
+    }, 150)
+  }
+  calculatePosition() {
+    let rad = (this.rotation - 90) * (Math.PI / 180)
+
+    this.translateX = Math.min(
+      Math.max(this.translateX + this.speed * Math.cos(rad), this.minX),
+      this.maxX,
+    )
+
+    this.translateY = Math.min(
+      Math.max(this.translateY + this.speed * Math.sin(rad), this.minY),
+      this.maxY,
+    )
+  }
+  transform() {
+    this.beeNode.style.transform =
+      'translate(' +
+      this.translateX +
+      'px, ' +
+      this.translateY +
+      'px) rotate(' +
+      this.rotation +
+      'deg)'
+  }
+  checkWallCollisions() {
+    if (
+      this.translateX == this.minX ||
+      this.translateY == this.minY ||
+      this.translateX == this.maxX ||
+      this.translateY == this.maxY
+    ) {
+      this.rotation += 90
+    }
+  }
+  stop() {
+    clearInterval(this.timer)
+  }
+  go() {
+    let fast = Math.random() * 2000 + 2000
+    let slow = Math.random() * 2000 + 1000
+    this.move(50, 30)
+    window.setInterval(() => {
+      this.FastMove(slow, 5, 70).then(() => this.FastMove(fast, 50, 30))
+    }, fast + slow)
+  }
+  async FastMove(t: number, s: number, r: number) {
+    await this.delay(t, s, r)
+  }
+  async delay(milliseconds: number, s: number, r: number) {
+    this.move(s, r)
+    // setTimeout(() => {
+    //   this.FastMove(4000, 50, 30)
+    // }, milliseconds)
+    return new Promise(resolve => {
+      setTimeout(() => {
+        resolve()
+      }, milliseconds)
+    })
+  }
+}
+
+let bees = []
+// generate bees
+for (var i = 0; i < number_of_bees; ++i) {
+  let tmp = makeDiv('bee')
+  workspace.appendChild(tmp)
+  bees.push(tmp)
+}
+
+// let them buzz
+for (var i = 0; i < bees.length; ++i) {
+  let b = bees[i] as HTMLElement
+  let t = new Bee(b, workspace)
+  t.go()
+}
---
+++ gulpfile.js
@@ -0,0 +1,47 @@
+const gulp = require('gulp'),
+  bs = require('browser-sync'),
+  pug = require('gulp-pug'),
+  sass = require('gulp-sass'),
+  ts = require('gulp-typescript')
+
+gulp.task('serve', () => {
+  bs.init({
+    server: {
+      baseDir: './dist',
+    },
+  })
+  gulp.watch('app/scss/*.scss', ['scss'])
+  gulp.watch('app/pug/**/*.pug', ['pug'])
+  gulp.watch('app/ts/**/*.ts', ['ts'])
+  gulp.watch('dist/**/*.html').on('change', bs.reload)
+})
+
+gulp.task('scss', () => {
+  return gulp
+    .src('app/scss/**/*.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('dist/css'))
+    .pipe(bs.stream())
+})
+
+gulp.task('pug', () => {
+  return gulp
+    .src('app/pug/**/*.pug')
+    .pipe(pug())
+    .pipe(gulp.dest('dist'))
+    .pipe(bs.stream())
+})
+
+gulp.task('ts', () => {
+  return gulp
+    .src('app/ts/**/*.ts')
+    .pipe(
+      ts({
+        noImplicitAny: true,
+        outFile: 'main.js',
+        target: 'ESNext',
+      }),
+    )
+    .pipe(gulp.dest('dist/js'))
+    .pipe(bs.stream())
+})
